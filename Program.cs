42fa4e37631de0bfb36e558ab37123e09389aad7
nickdizes <nick.dizes18@gmail.com>
2024-01-22 10:26:10 -0700

    new class

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
         //holds however many rolls that the user wants to simulate
         int diceRolls = 0;
@@ -21,8 +21,11 @@ internal class Program
                             "Each \"*\" represent 1% of the total number of rolls\n" +
                             "Total number of rolls = " + diceRolls + "\n");
 
+        //makes a new class object
+        RollDice rolldice = new RollDice();
+       
         //calls the function that actually rolls the dice
-        results = RollDice(diceRolls);
+        results = rolldice.Simulator(diceRolls);
 
         //loops through the results and dynamically makes the histogram
         for (int i = 0; i < results.Length; i++)
@@ -34,36 +37,6 @@ internal class Program
 
         Console.WriteLine();
         Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
-    }
-
-    private static int[] RollDice(int diceRolls)
-    {
-        Random random = new Random();
-
-        //variables to hold what number was rolled 
-        int diceOne = 0;
-        int diceTwo = 0;
-        int total = 0;
-        int maxTotal = 12;
-        //sets how many sided dice it is
-        int min = 1;
-        int max = 7;
-
-        //arrary to hold results
-        int[] results = new int[maxTotal - 1];
-
-        //simulates however many rolls that the user wants to do 
-        for (int i = 0; i < diceRolls; i++)
-        {
-            diceOne = random.Next(min, max);
-            diceTwo = random.Next(min, max);
-            total = diceOne + diceTwo;
-
-            //it is the total - 2 since the lowest possible roll is a 2
-            results[total - 2] = results[total - 2] + 1;
-        }
-
-        return results;
     }
 }
 
---
+++ RollDice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class RollDice
+{
+    public int[] Simulator(int diceRolls)
+    {
+        Random random = new Random();
+
+        //variables to hold what number was rolled 
+        int diceOne = 0;
+        int diceTwo = 0;
+        int total = 0;
+        int maxTotal = 12;
+        //sets how many sided dice it is
+        int min = 1;
+        int max = 7;
+
+        //arrary to hold results
+        int[] results = new int[maxTotal - 1];
+
+        //simulates however many rolls that the user wants to do 
+        for (int i = 0; i < diceRolls; i++)
+        {
+            diceOne = random.Next(min, max);
+            diceTwo = random.Next(min, max);
+            total = diceOne + diceTwo;
+
+            //it is the total - 2 since the lowest possible roll is a 2
+            results[total - 2] = results[total - 2] + 1;
+        }
+
+        return results;
+    }
+}
